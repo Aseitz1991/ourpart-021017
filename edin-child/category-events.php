c7769c19c89952a9fdc6d3e6533b05c116e94315
Aseitz1991 <aseitz6@gmail.com>
2017-02-10 15:17:03 -0500

    updates to category-events & content-events page

--- edin-child/category-events.php
+++ edin-child/category-events.php
@@ -1,15 +1,7 @@
 <?php
-/**
- * The template for displaying Archive pages.
- *
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package Edin
- */
-
 get_header(); ?>
 
-	TEST
+	<!-- this is just for the header of Category (ex: Activities, Events & Meetups) -->
 
 	<div class="hero <?php echo edin_additional_class(); ?>">
 		<?php if ( have_posts() ) : ?>
@@ -19,70 +11,15 @@ get_header(); ?>
 					<?php
 						if ( is_category() ) :
 							single_cat_title();
-
-						elseif ( is_tag() ) :
-							single_tag_title();
-
-						elseif ( is_author() ) :
-							printf( __( 'Author: %s', 'edin' ), '<span class="vcard">' . get_the_author() . '</span>' );
-
-						elseif ( is_day() ) :
-							printf( __( 'Day: %s', 'edin' ), '<span>' . get_the_date() . '</span>' );
-
-						elseif ( is_month() ) :
-							printf( __( 'Month: %s', 'edin' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'edin' ) ) . '</span>' );
-
-						elseif ( is_year() ) :
-							printf( __( 'Year: %s', 'edin' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'edin' ) ) . '</span>' );
-
-						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-							_e( 'Asides', 'edin' );
-
-						elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
-							_e( 'Galleries', 'edin');
-
-						elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-							_e( 'Images', 'edin');
-
-						elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-							_e( 'Videos', 'edin' );
-
-						elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-							_e( 'Quotes', 'edin' );
-
-						elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-							_e( 'Links', 'edin' );
-
-						elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
-							_e( 'Statuses', 'edin' );
-
-						elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
-							_e( 'Audios', 'edin' );
-
-						elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
-							_e( 'Chats', 'edin' );
-
-						else :
-							_e( 'Archives', 'edin' );
-
-						endif;
+					endif;
 					?>
 				</h1>
-				<?php
-					// Show an optional term description.
-					$term_description = term_description();
-					if ( ! empty( $term_description ) ) :
-						printf( '<div class="taxonomy-description">%s</div>', $term_description );
-					endif;
-				?>
 			</div>
 
 		<?php else : ?>
-
 			<div class="title-wrapper">
 				<h1 class="page-title"><?php _e( 'Nothing Found', 'edin' ); ?></h1>
 			</div>
-
 		<?php endif; ?>
 	</div><!-- .hero -->
 
@@ -96,12 +33,14 @@ get_header(); ?>
 					<?php /* Start the Loop */ ?>
 					<?php while ( have_posts() ) : the_post(); ?>
 
+						<!--content-events.php file test - 021017 -->
+
 						<?php
 							/* Include the Post-Format-specific template for the content.
 							 * If you want to override this in a child theme, then include a file
 							 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 							 */
-							get_template_part( 'content', get_post_format() );
+							get_template_part( 'content-events', get_post_format() );
 						?>
 
 					<?php endwhile; ?>
