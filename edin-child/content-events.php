0f183551d51b31505d4580d7254e0836d9146b5d
Aseitz1991 <aseitz6@gmail.com>
2017-02-12 14:19:50 -0500

    Addition of Like & Share Bars In Content

--- edin-child/content-events.php
+++ edin-child/content-events.php
@@ -8,13 +8,36 @@
 <div class="fullpost">
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		
+		<div class="sharebar">
+				<!-- <h4> Share Button & Like Bar </h4> -->
+				<!-- I got these buttons from simplesharebuttons.com -->
+				<div id="share-buttons">
+				    <!-- Facebook -->
+				    <a href="http://www.facebook.com/sharer.php?u=https://simplesharebuttons.com" target="_blank">
+				        <img src="https://simplesharebuttons.com/images/somacro/facebook.png" alt="Facebook" />
+				    </a>
+				   
+				    <!-- Pinterest -->
+				    <a href="javascript:void((function()%7Bvar%20e=document.createElement('script');e.setAttribute('type','text/javascript');e.setAttribute('charset','UTF-8');e.setAttribute('src','http://assets.pinterest.com/js/pinmarklet.js?r='+Math.random()*99999999);document.body.appendChild(e)%7D)());">
+				        <img src="https://simplesharebuttons.com/images/somacro/pinterest.png" alt="Pinterest" />
+				    </a>
+
+				    <!-- Twitter -->
+				    <a href="https://twitter.com/share?url=https://simplesharebuttons.com&amp;text=Simple%20Share%20Buttons&amp;hashtags=simplesharebuttons" target="_blank">
+				        <img src="https://simplesharebuttons.com/images/somacro/twitter.png" alt="Twitter" />
+				    </a>
+				</div>
+
+				<!-- LikeBtn.com BEGIN -->
+					<span class="likebtn-wrapper" data-theme="custom" data-icon_l="hrt6" data-icon_l_c_v="#2705fb" data-identifier="item_1" data-dislike_enabled="false" data-icon_dislike_show="false" data-popup_disabled="true"></span>
+					
+					<script>(function(d,e,s){if(d.getElementById("likebtn_wjs"))return;a=d.createElement(e);m=d.getElementsByTagName(e)[0];a.async=1;a.id="likebtn_wjs";a.src=s;m.parentNode.insertBefore(a, m)})(document,"script","//w.likebtn.com/js/w/widget.js");</script>
+					<!-- LikeBtn.com END -->
+		</div>
+
 		<header class="entry-header">
 
-			<div class="sharebar">
-				<h4> Share Button & Like Bar </h4>
-			</div>
-
-			<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( edin_get_link_url() ) ), '</a></h2>' ); ?>
+				<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( edin_get_link_url() ) ), '</a></h2>' ); ?>
 
 			<?php if ( 'post' == get_post_type() ) : ?>
 			<div class="entry-meta">
@@ -22,7 +45,10 @@
 			</div><!-- .entry-meta -->
 			<?php endif; ?>
 
-			<div class="fixedimage">
+			<!-- The Fixed Image -->
+
+			
+		<div class="fixedimage">
 
 				<?php
 					if ( 'post' == get_post_type() ) {
@@ -31,11 +57,14 @@
 				?>
 
 			</div>
+
+
+
 		</header><!-- .entry-header -->
 
 
 
-		<!-- HIDING THE FOLLOWING CONTENT: MAP,
+		<!-- HIDING THE FOLLOWING CONTENT: MAP, AGE, DESCRIPTION
 
 		<div class="entry-content">
 			<?php
@@ -53,6 +82,14 @@
 				) );
 			?>
 		</div><!-- .entry-content -->
+
+		<!-- This is where the Descriptions under the titles go -->
+
+		<div class="description">
+
+		<?php echo get_post_meta( $post->ID, 'date', true ); ?> <br>
+
+		</div>
 
 		<footer class="entry-footer">
 			<?php edin_entry_meta(); ?>
