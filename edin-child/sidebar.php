9dab99fc58634623a326c66965de13e6d1978dab
Aseitz1991 <aseitz6@gmail.com>
2017-02-13 17:30:26 -0500

    updates to header & log-in

--- edin-child/sidebar.php
+++ edin-child/sidebar.php
@@ -6,9 +6,7 @@
  */
 ?>
 
-	<li><?php printf( __( 'Hello %s', 'wpuf' ), $user->display_name ); ?></li>
-
-	<?php printf( __( '%s', 'wpuf' ), wp_loginout( '', false ) ) ?>
+	<li><?php printf( __( 'Hello %s', 'wpuf' ), $user->user_name ); ?></li>
 
 	<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 	<div id="secondary" class="widget-area" role="complementary">
